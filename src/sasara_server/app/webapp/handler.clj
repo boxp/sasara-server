b400b3c8381d23ba99ce68a7c3a64f157b49e382
boxp <tiyotiyouda@gmail.com>
2017-12-21 18:15:23 +0900

    bump up clojure version

--- src/sasara_server/app/webapp/handler.clj
+++ src/sasara_server/app/webapp/handler.clj
@@ -2,29 +2,22 @@
   (:require [clojure.spec.alpha :as s]
             [com.stuartsierra.component :as component]
             [cheshire.core :refer [generate-string]]
-            [sasara-server.domain.usecase.speak :as speak]))
-
-(s/def ::status #{200 404 408 204})
-(s/def ::headers (s/map-of string? string?))
-(s/def ::body string?)
-(s/def ::response
-  (s/keys :req-un [::status ::headers ::body]))
-(s/def ::webapp-handler-component
-  (s/keys :req-un [:speak/speak-usecase-component]))
+            [sasara-server.domain.usecase.speak :as speak]
+            [sasara-server.app.webapp.handler-spec :as spec]))
 
 (s/fdef index
-  :args (s/cat :comp ::webapp-handler-component)
-  :ret ::response)
+  :args (s/cat :comp ::spec/webapp-handler-component)
+  :ret ::spec/response)
 (defn index
   [comp]
   {:status 200
    :headers {"Content-Type" "text/html; charset=utf-8"}
-   :body "Hello!(｀･ω･´)\n"})
+   :body "Hello!World!\n"})
 
 (s/fdef speak
-  :args (s/cat :comp ::webapp-handler-component
+  :args (s/cat :comp ::spec/webapp-handler-component
                :message string?)
-  :ret ::response)
+  :ret ::spec/response)
 (defn speak
   [{:keys [speak-usecase-component] :as comp}
    message]
@@ -36,7 +29,7 @@
                  generate-string)}
       {:status 408
        :headers {"Content-Type" "application/json; charset=utf-8"}
-       :body (-> {:message "Request Timeout."}
+       :body (-> {:message "タイムアウトしました。もう一度お試しください。"}
                  generate-string)})))
 
 (defrecord WebappHandlerComponent []
